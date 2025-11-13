306d66b5ae37d9ee2f08fbdb2286f8984e5d7f2e
Pranjal Singh ATTPC <pranjalsingh31299@gmail.com>
2025-11-13 16:53:52 -0500

    13112025 filter class

---
+++ scripts/filtter_and_combine.py
@@ -0,0 +1,25 @@
+class Filter:
+    
+    def __init__(self):
+        pass
+    
+    def h5_keys_extract(self, file_path: str):
+        """Extracts the data from an HDF5 file.
+
+        Args:
+            file_path (string): String path to the HDF5 file.
+        Returns:
+            data_keys(): keys of the data in the HDF5 file.
+        """
+        
+        
+        
+        
+    def check_nans(self, data):
+        """Check for any NaN values in the data.
+
+        Args:
+            data (_type_): _description_
+        """
+        
+        
