9a58447dd131560f3bdeb8ae17dfe44b558369e7
Pranjal Singh ATTPC <pranjalsingh31299@gmail.com>
2025-12-11 15:11:25 -0500

    fixed parameters

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -6,8 +6,8 @@ def test_outlier_detection1():
     data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[50,60,70,80],[-150,25,350,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
     #
     event_length = np.array([3,5,2])
-    outlier_detector = OutlierDetection(data)
-    outlier_removed, new_event_len = outlier_detector.transform(data, event_length)
+    outlier_detector = OutlierDetection()
+    outlier_removed, new_event_len = outlier_detector.transform((data, event_length))
     
     assert  data.shape == outlier_removed.shape #to check if the shape is the same as there are no outliers in the data
     assert np.array_equal(event_length, new_event_len) #event lengths should be the same since no points are removed
@@ -18,7 +18,7 @@ def test_outlier_detection2():
     
     event_length = np.array([3,5,2])
     outlier_detector = OutlierDetection(data)
-    outlier_removed, new_event_len = outlier_detector.transform(data, event_length)
+    outlier_removed, new_event_len = outlier_detector.transform((data, event_length))
     
     assert data.shape != outlier_removed.shape #to check if the shape is different as there are outliers in the data
     assert np.all(new_event_len <= event_length) #new event lengths should be less than
@@ -33,8 +33,8 @@ def test_up_down_scaling():
     target_size = 5
     isotope = "test_isotope"
     
-    updown_sampler = UpDownScaling()
-    updownsampled_data, updownsampled_event_len = updown_sampler.transform(data, event_length, target_size, isotope)
+    updown_sampler = UpDownScaling(target_size, isotope)
+    updownsampled_data, updownsampled_event_len = updown_sampler.transform((data, event_length))
     
     assert updownsampled_data.shape[0] >= data.shape[0] #number of events should be same or more after up/down sampling
     assert np.all(updownsampled_event_len == target_size + 2) #all event lengths should be equal to target size
