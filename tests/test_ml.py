3764aa9fa5025606f2ddcd3d63215a62008f1fa3
Pranjal Singh ATTPC <pranjalsingh31299@gmail.com>
2025-12-11 16:37:17 -0500

    11122025 all tests bugs fixed

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def test_outlier_detection1():
     data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[50,60,70,80],[-150,25,350,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
-    #
+    
     event_length = np.array([3,5,2])
     outlier_detector = OutlierDetection()
     outlier_removed, new_event_len = outlier_detector.transform((data, event_length))
@@ -22,12 +22,13 @@ def test_outlier_detection2():
     
     assert outlier_removed.shape == data.shape
     assert np.all(new_event_len <= event_length) #new event lengths should be less than
+
     
 def test_up_down_scaling():
     data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
     event_length = np.array([3,5,2])
     target_size = 5
-    isotope = "test_isotope"
+    isotope = "O16"
     
     updown_sampler = UpDownScaling(target_size, isotope)
     updownsampled_data = updown_sampler.transform((data, event_length))
