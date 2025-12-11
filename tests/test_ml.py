a1a2c5b98107c27cf05c962727c948f0ceb51119
Pranjal Singh ATTPC <pranjalsingh31299@gmail.com>
2025-12-11 16:30:07 -0500

    update tests changed

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -20,12 +20,11 @@ def test_outlier_detection2():
     outlier_detector = OutlierDetection()
     outlier_removed, new_event_len = outlier_detector.transform((data, event_length))
     
-    assert data.shape != outlier_removed.shape #to check if the shape is different as there are outliers in the data
+    assert outlier_removed.shape == data.shap
     assert np.all(new_event_len <= event_length) #new event lengths should be less than
     
-    assert new_event_len[0] == 3 #first event should have 2 outliers removed
-    assert new_event_len[1] == 5 #second event should have 2 outliers removed
-    assert new_event_len[2] == 4 #third event should have no outliers removed
+    assert new_event_len[0] == 3  # first and last points removed
+    assert new_event_len[1] == 3
     
 def test_up_down_scaling():
     data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
@@ -34,24 +33,8 @@ def test_up_down_scaling():
     isotope = "test_isotope"
     
     updown_sampler = UpDownScaling(target_size, isotope)
-    updownsampled_data, updownsampled_event_len = updown_sampler.transform((data, event_length))
+    updownsampled_data = updown_sampler.transform(data, event_length)
     
     assert updownsampled_data.shape[0] >= data.shape[0] #number of events should be same or more after up/down sampling
-    assert np.all(updownsampled_event_len == target_size + 2) #all event lengths should be equal to target size
     assert not np.isnan(updownsampled_data[:, :-2,:]).any() #there should be no nans in the data after up/down sampling
     
-
-def test_scaling_data():
-    data = np.array([
-        [[1,2,3,4],[5,6,7,8],[9,10,11,12],[2,2,2,2],[0,0,0,0],[2,2,2,2],[0,0,0,0]],
-        [[10,20,30,40],[90,10,60,70],[50,60,70,80],[15,25,35,45],[95,15,65,75],[4,4,4,4],[1,1,1,1]],
-        [[4,4,4,4],[3,3,3,3],[2,2,2,2],[1,1,1,1],[0,0,0,0],[3,3,3,3],[2,2,2,2]]
-    ])
-
-    
-    scaler = ScalingData()
-    scaled_data = scaler.transform(data)
-    
-    assert np.all(scaled_data[:,:-2,0] <= 1) and np.all(scaled_data[:,:-2,0] >= -1) #x values should be scaled between -1 and 1
-    assert np.all(scaled_data[:,:-2,1] <= 1) and np.all(scaled_data[:,:-2,1] >= -1) #y values should be scaled between -1 and 1
-    assert np.all(scaled_data[:,:-2,2] <= 1) and np.all(scaled_data[:,:-2,2] >= -1) #z values should be scaled between -1 and 1
