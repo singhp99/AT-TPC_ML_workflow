2ed4897386fc8b0e3017d0e3a4c467c644b3ff59
Pranjal Singh ATTPC <pranjalsingh31299@gmail.com>
2025-12-11 15:08:59 -0500

    fixed inhomogeneous array

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def test_outlier_detection1():
-    data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan],[np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[50,60,70,80],[-150,25,350,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan],[np.nan,np.nan],[np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
+    data = np.array([[[-200,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[50,60,70,80],[-150,25,350,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
     #
     event_length = np.array([3,5,2])
     outlier_detector = OutlierDetection(data)
@@ -14,7 +14,7 @@ def test_outlier_detection1():
     
     
 def test_outlier_detection2():
-    data = np.array([[[-1000,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan],[np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan],[np.nan,np.nan],[np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
+    data = np.array([[[-1000,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
     
     event_length = np.array([3,5,2])
     outlier_detector = OutlierDetection(data)
@@ -28,7 +28,7 @@ def test_outlier_detection2():
     assert new_event_len[2] == 4 #third event should have no outliers removed
     
 def test_up_down_scaling():
-    data = np.array([[[-1000,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan],[np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan],[np.nan,np.nan],[np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
+    data = np.array([[[-1000,14,500,1000],[5,6,7,8],[9,10,11,12],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[2,2,2,2],[1,1,1,1]],[[10,20,30,40],[90,10,60,70],[15,600,70,80],[-150,25,2000,450],[-80,15,250,300],[1,1,1,1],[2,2,2,2]],[[15,25,35,45],[95,15,65,75],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[np.nan,np.nan,np.nan,np.nan],[4,4,4,4],[3,3,3,3]]])
     event_length = np.array([3,5,2])
     target_size = 5
     isotope = "test_isotope"
